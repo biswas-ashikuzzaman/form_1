3c74a0ebc6716ed1743e0ee823c0b75f4bde87b6
hiclassic <biswasashikuzzaman@gmail.com>
2025-06-25 11:02:27 +0600

    update

--- practice_opp_form.js
+++ practice_opp_form.js
@@ -9,8 +9,17 @@ class FormValues {
         result +="last Name:" +this.sln + "<br>";
         result +="Student ID:" +this.sid + "<br>";
         result +="Gender:" +this.sgender + "<br>";
-        result +="last Name:" +this.sln + "<br>";
+        result +="Course:" +this.scourse + "<br>";
+        var win= window.open('','' 'width=500,height=500');
+        win.document.write(result);
 
 
     }
+}
+form1.onsubmit=async (e)=> {
+    e.preventDefault();
+    var fm=document.querySelector('#form1');
+    var form_data=new FormData(fm);
+    var f= new FormValues(form_data);
+    f.getDisplay();
 }
